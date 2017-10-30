82cfe48338d051cf5513f82fcefa3b0e55dc7b66
MikeGoz <gozdz@onet.pl>
2017-10-30 19:56:24 +0100

    Task 7.3

---
+++ actions.js
@@ -0,0 +1,53 @@
+// Task 7.3
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+  return {
+      type: ADD_COMMENT,
+      text,
+      id: uuid.v4()
+  }
+}
+function editComment(id) {
+  return {
+      type: EDIT_COMMENT,
+      text,
+      id: id
+  }
+}
+function removeComment(id) {
+  return {
+      type: REMOVE_COMMENT,
+      id: id
+  }
+}
+function thumbUpComment(id) {
+  return {
+      type: THUMB_UP_COMMENT,
+      id: id,
+      img_src = ''
+  }
+}
+function thumDownComment(id) {
+  return {
+      type: THUMB_DOWN_COMMENT,
+      id: id,
+      img_src = ''
+  }
+}
+const boundAddComment = text => dispatch(addComment(text));
+const boundEditomment = id => dispatch(addComment(id));
+const boundRemoveComment = id => dispatch(addComment(id));
+const boundThumbUpComment = id => dispatch(addComment(id));
+const boundThumDownComment = id => dispatch(addComment(id));
+
+boundAddComment('New comment!');
+boundEditomment('Changed comment');
+boundRemoveComment(123);
+boundThumbUpComment(321);
+boundThumDownComment(345);
